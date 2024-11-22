6181e4ac21712414fe7035b29e4f72e48a5d5679
GlauberJr <0000109387949xsp@al.educacao.sp.gov.br>
2024-11-22 15:17:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Em que ano o Corinthians quanhou sua primeira Libertadores?',
+    'O Corinthians quanhou sua primeira Libertadores em 2012'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual capital da Austrália?',
+    'A capital da Austrália é Camberra'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é o maior campeão da Champions League?',
+    ' O maior campeão da Champions League é o Real Madri'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ir em Inglês?',
+    'Ir em ingles é Go'
+)
